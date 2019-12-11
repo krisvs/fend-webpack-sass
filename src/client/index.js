ed59d49c22d931863ceb8d8c8dbbddedd2d4f1e3
Kris Van Sebroeck <krisvs@gmail.com>
2019-12-11 13:03:23 +0000

    Many changes

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,17 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+export {
+    checkForName,
+    handleSubmit
+}
+
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
+
 console.log(checkForName);
 
 alert("I EXIST")
